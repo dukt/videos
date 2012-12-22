2787911fc6bf798c94ebee441be8d2d7d5b3ffa0
Benjamin David <ben@dukt.net>
2012-12-22 04:14:38 +0100

    added records

---
+++ records/DuktVideos_OptionRecord.php
@@ -0,0 +1,18 @@
+<?php
+namespace Blocks;
+
+class DuktVideos_OptionRecord extends BaseRecord
+{
+    public function getTableName()
+    {
+        return 'duktvideos_options';
+    }
+
+    public function defineAttributes()
+    {
+        return array(
+            'option_name' => AttributeType::String,
+            'option_value' => AttributeType::String,
+        );
+    }
+}
