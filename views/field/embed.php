c45903f04fb8895df20a2b14185d5ed0bc7f48ee
Benjamin David <ben@dukt.net>
2012-12-22 04:15:13 +0100

    added views

---
+++ views/field/embed.php
@@ -0,0 +1,5 @@
+<div class="dukt-videos-field-embed">
+	<?php
+	echo $video['embed'];
+	?>
+</div>
---
+++ views/field/preview.php
@@ -0,0 +1,7 @@
+<a data-method="field_embed" data-video-page="<?php echo $video['url']?>" class="dukt-videos-field-embed-btn">
+	<?php
+	echo $video['embed'];
+	?>
+</a>
+
+
