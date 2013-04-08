e2ba9bf8225c9abdcc73c014bbb8f36ba9565e90
Benjamin David <ben@dukt.net>
2013-04-08 11:49:21 +0200

    Added : Modal for field

--- resources/js/field.js
+++ resources/js/field.js
@@ -22,24 +22,61 @@ console.log('hello field.js');
 
     $.fn.dukt_videos_field.init = function()
     {
+         $.fn.dukt_videos_field.modal.init();
 
-        // get modal
+
+        // get modal body
 
         Craft.postActionRequest('duktvideos/ajax/modal', {}, function(response) {
 
+            // load modal body
+
             $(response).appendTo('body');
+
+
+
             
-                // manual bootstrap
-                
-                angular.bootstrap(document, ['duktvideos']);
+            // manual bootstrap
+            
+            angular.bootstrap(document, ['duktvideos']);
         });
 
+    };
+
+    $.fn.dukt_videos_field.modal = {
+        init: function() {
+            console.log('videos.modal.init()');
+
+            overlay = $('<div class="dv-overlay"></div>');
+
+            overlay.appendTo('body');
+
+            overlay.click(function() {
+                $.fn.dukt_videos_field.modal.hide();
+            });
+        },
+
+        resize: function() {
+
+        },
+
+        show : function() {
+            $('.dv-overlay').css('display', 'block');
+            $('.dv-modal').css('display', 'block');
+        },
+
+        hide: function() {
+            $('.dv-overlay').css('display', 'none');
+            $('.dv-modal').css('display', 'none');
+        }
     };
 
 
     $.fn.dukt_videos_field.init_field = function(field)
     {
-
+        $('.add', field).click(function() {
+            $.fn.dukt_videos_field.modal.show();
+        });
     }
     
     // Initialization
