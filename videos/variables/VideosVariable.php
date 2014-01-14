a7eb23ea9aa4de3022983b93d02b09f4e85af61e
Benjamin David <ben@dukt.net>
2014-01-14 08:17:29 +0100

    Added craft.videos.getVideoByUrl(url, enableCache, cacheExpiry)

--- videos/variables/VideosVariable.php
+++ videos/variables/VideosVariable.php
@@ -4,8 +4,12 @@ namespace Craft;
 
 class VideosVariable
 {
-    public function getVideoByUrl($videoUrl)
+    public function getVideoByUrl($videoUrl, $enableCache = true, $cacheExpiry = 3600)
     {
-        return craft()->videos->getVideoByUrl($videoUrl);
+        try {
+            return craft()->videos->getVideoByUrl($videoUrl, $enableCache, $cacheExpiry);
+        } catch(\Exception $e) {
+            Craft::log("Couldn't get video from URL : ".$videoUrl.'. '.$e->getMessage(), LogLevel::Info, true);
+        }
     }
 }
