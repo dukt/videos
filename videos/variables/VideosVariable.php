6e536c0866b1372bcbd4ec628e2e3f239a238765
Benjamin David <ben@dukt.net>
2014-02-18 18:15:00 +0100

    Videos variables displaying errors only in devMode

--- videos/variables/VideosVariable.php
+++ videos/variables/VideosVariable.php
@@ -7,14 +7,20 @@ class VideosVariable
     public function getVideoByUrl($videoUrl, $enableCache = true, $cacheExpiry = 3600)
     {
         try {
-
             $video = craft()->videos->getVideoByUrl($videoUrl, $enableCache, $cacheExpiry);
 
             return $video;
         }
         catch(\Exception $e)
         {
-            Craft::log("Couldn't get video from URL : ".$videoUrl.'. '.$e->getMessage(), LogLevel::Info, true);
+            if(craft()->config->get('devMode'))
+            {
+                throw $e;
+            }
+            else
+            {
+                Craft::log("Couldn't get video from URL : ".$videoUrl.'. '.$e->getMessage(), LogLevel::Info, true);
+            }
         }
     }
 }
