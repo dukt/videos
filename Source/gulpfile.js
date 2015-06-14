0cebda98483900ac03746e2055688963cad6c005
Benjamin David <ben@dukt.net>
2015-06-14 17:01:59 +0200

    Added gulp

---
+++ Source/gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    livereload = require('gulp-livereload'),
+    del = require('del'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
+
+var paths = {
+    less: './videos/resources/less',
+    css: './videos/resources/css',
+    js: './videos/resources/js',
+    jsCompressed: './videos/resources/js/compressed',
+}
+
+/* Less */
+
+gulp.task('less', function () {
+  return gulp.src(paths.less+'/**/*.less')
+    .pipe(less({
+      paths: []
+    }))
+    .pipe(gulp.dest(paths.css));
+});
+
+
+/* JS */
+
+gulp.task('scripts', function() {
+    return gulp.src([
+        paths.js+'/*.js'
+    ])
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.jsCompressed));
+});
+
+/* Clean */
+
+gulp.task('clean', function(cb) {
+    del([paths.css, paths.jsCompressed], cb)
+});
+
+
+/* Default Task */
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('less', 'scripts');
+});
+
+
+/* Watch */
+
+gulp.task('watch', function() {
+
+    gulp.watch(paths.less+'/**/*.less', ['less']);
+    gulp.watch(paths.js+'/*.js', ['scripts']);
+
+    livereload.listen();
+
+    gulp.watch([paths.css+'/**', paths.jsCompressed]).on('change', livereload.changed);
+
+});
