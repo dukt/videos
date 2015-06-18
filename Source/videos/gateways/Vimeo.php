6205aca51b085524613c4482d64faafb4c7df29f
Benjamin David <ben@dukt.net>
2015-06-18 12:13:01 +0200

    Added Vimeo and YouTube gateways

---
+++ Source/videos/gateways/Vimeo.php
@@ -0,0 +1,434 @@
+<?php
+namespace Dukt\Videos\Gateways;
+
+class Vimeo extends BaseGateway
+{
+    public $oauthProvider = 'Vimeo';
+    public $oauthScope    = array();
+
+    // Public Methods
+    // =========================================================================
+
+    public function getName()
+    {
+        return "Vimeo";
+    }
+
+    public function getSections()
+    {
+        $sections = array();
+        $sections['Library'] = array(
+            array(
+                'name' => "Uploads",
+                'method' => 'uploads'
+            ),
+            array(
+                'name' => "Favorites",
+                'method' => 'favorites'
+            )
+        );
+
+
+        // albums
+
+        $albums = $this->getCollectionsAlbums();
+
+        if(is_array($albums))
+        {
+            $items = array();
+
+            foreach($albums as $album)
+            {
+                $item = array(
+                    'name' => $album['title'],
+                    'method' => 'album',
+                    'options' => array('id' => $album['id'])
+                );
+
+                $items[] = $item;
+            }
+
+            if(count($items) > 0)
+            {
+                $sections['Albums'] = $items;
+            }
+        }
+
+
+        // channels
+
+        $channels = $this->getCollectionsChannels();
+
+        if(is_array($channels))
+        {
+            $items = array();
+
+            foreach($channels as $channel)
+            {
+                $item = array(
+                    'name' => $channel['title'],
+                    'method' => 'channel',
+                    'options' => array('id' => $channel['id'])
+                );
+
+                $items[] = $item;
+            }
+
+            if(count($items) > 0)
+            {
+                $sections['Channels'] = $items;
+            }
+        }
+
+        return $sections;
+    }
+
+    // Protected Methods
+    // =========================================================================
+
+    protected static function getVideoId($url)
+    {
+        // check if url works with this service and extract video_id
+
+        $video_id = false;
+
+        $regexp = array('/^https?:\/\/(www\.)?vimeo\.com\/([0-9]*)/', 2);
+
+        if(preg_match($regexp[0], $url, $matches, PREG_OFFSET_CAPTURE) > 0)
+        {
+
+            // regexp match key
+            $match_key = $regexp[1];
+
+
+            // define video id
+            $video_id = $matches[$match_key][0];
+
+
+            // Fixes the youtube &feature_gdata bug
+            if(strpos($video_id, "&"))
+            {
+                $video_id = substr($video_id, 0, strpos($video_id, "&"));
+            }
+        }
+
+        // here we should have a valid video_id or false if service not matching
+        return $video_id;
+    }
+
+    protected function getEmbedFormat()
+    {
+        return "https://player.vimeo.com/video/%s";
+    }
+
+    protected function getBoolParameters()
+    {
+        return array('portrait', 'title', 'byline');
+    }
+
+    protected function getVideosAlbum($params = array())
+    {
+        $params['album_id'] = $params['id'];
+        unset($params['id']);
+
+         // albums/#album_id
+        return $this->_getVideosRequest('/me/albums/'.$params['album_id'].'/videos', $params);
+    }
+
+    protected function getVideosChannel($params = array())
+    {
+        $params['channel_id'] = $params['id'];
+        unset($params['id']);
+
+        return $this->_getVideosRequest('/channels/'.$params['channel_id'].'/videos', $params);
+    }
+
+    protected function getVideosSearch($params = array())
+    {
+        return $this->_getVideosRequest('/videos', $params);
+    }
+
+    protected function getVideosUploads($params = array())
+    {
+        return $this->_getVideosRequest('/me/videos', $params);
+    }
+
+    protected function getVideosFavorites($params = array())
+    {
+        return $this->_getVideosRequest('/me/likes', $params);
+    }
+
+    protected function getVideo($opts)
+    {
+        $method = '/videos/'.$opts['id'];
+        $response = $this->_api($method);
+
+        if(!empty($response['body']))
+        {
+            if(!isset($response['body']['error']))
+            {
+                return $this->parseVideo($response['body']);
+            }
+            else
+            {
+                throw new \Exception($response['body']['error'], 1);
+            }
+        }
+    }
+
+    // Private Methods
+    // =========================================================================
+
+    private function _getVideosRequest($uri, $params, $requireAuthentication = true)
+    {
+        $query = $this->_queryFromParams($params);
+
+        $response = $this->_api($uri, $query);
+
+        $videosRaw = $response['body']['data'];
+        $videos = $this->extractVideos($videosRaw);
+
+        $more = true;
+
+        if(count($videos) < $this->paginationDefaults['perPage'])
+        {
+            $more = false;
+        }
+
+        return array(
+                'videos' => $videos,
+                'nextPage' => $query['nextPage'],
+                'more' => $more
+            );
+    }
+
+    private function extractVideos($r)
+    {
+        $videos = array();
+
+        if(!empty($r))
+        {
+            $responseVideos = $r;
+
+            foreach($responseVideos as $responseVideo)
+            {
+                $video = $this->parseVideo($responseVideo);
+
+                array_push($videos, $video);
+            }
+        }
+
+        return $videos;
+    }
+
+    private function _api($method, $params = array())
+    {
+        $token = $this->token;
+        // client id & secret are fake because we already have a valid token
+        $vimeo = new \Dukt\Vimeo(
+            "clientId",
+            "clientSecret",
+            $token->accessToken
+        );
+
+        $return = array();
+
+        try
+        {
+            $return = $vimeo->request($method, $params);
+        }
+        catch(\Exception $e)
+        {
+            if($e->getMessage() != 'Page out of bounds')
+            {
+                throw $e;
+            }
+        }
+
+        return $return;
+    }
+
+    private function performRequest($uri, $params, $requireAuthentication = true)
+    {
+        $query = $this->_queryFromParams($params);
+
+        $response = $this->_api($uri, $query);
+
+        $videosRaw = $response['body']['data'];
+        $videos = $this->extractVideos($videosRaw);
+
+        $more = true;
+
+        if(count($videos) < $this->paginationDefaults['perPage'])
+        {
+            $more = false;
+        }
+
+        return array(
+                'videos' => $videos,
+                'nextPage' => $query['nextPage'],
+                'more' => $more
+            );
+    }
+
+    private function parseCollectionAlbum($response)
+    {
+        if($response)
+        {
+            $collection = array();
+            $collection['id'] = substr($response['uri'], (strpos($response['uri'], '/albums/') + strlen('/albums/')));
+            $collection['url'] = $response['uri'];
+            $collection['title'] = $response['name'];
+            $collection['totalVideos'] = $response['stats']['videos'];
+
+            return $collection;
+        }
+    }
+
+    private function parseCollectionChannel($response)
+    {
+        if($response)
+        {
+            $collection = array();
+            $collection['id'] = substr($response['uri'], (strpos($response['uri'], '/channels/') + strlen('/channels/')));
+            $collection['url'] = $response['uri'];
+            $collection['title'] = $response['name'];
+            $collection['totalVideos'] = $response['stats']['videos'];
+
+            return $collection;
+        }
+    }
+
+    private function getCollectionsAlbums($params = array())
+    {
+        $query = $this->_queryFromParams();
+
+        $response = $this->_api('/me/albums', $query);
+
+        return $this->extractCollections($response['body']['data'], 'album');
+    }
+
+    private function getCollectionsChannels($params = array())
+    {
+        $query = $this->_queryFromParams();
+
+        $response = $this->_api('/me/channels', $query);
+
+        return $this->extractCollections($response['body']['data'], 'channel');
+    }
+
+    private function parseUser()
+    {
+        $this->id = $response->id;
+        $this->name = $response->display_name;
+    }
+
+    private function parseVideo($item)
+    {
+        $video['raw'] = $item;
+
+        // populate video object
+        $video['authorName']    = $item['user']['name'];
+        $video['authorUrl']     = $item['user']['link'];
+        $video['date']          = strtotime($item['created_time']);
+        $video['description']   = $item['description'];
+        $video['gatewayHandle'] = "vimeo";
+        $video['gatewayName']   = "Vimeo";
+        $video['id']            = substr($item['uri'], strlen('/videos/'));
+        $video['plays']         = (isset($item['stats']['plays']) ? $item['stats']['plays'] : 0);
+        $video['title']         = $item['name'];
+        $video['url']           = $item['link'];
+
+
+        // duration
+        $video['durationSeconds'] = $item['duration'];
+        $video['duration']        = $this->getDuration($video['durationSeconds']);
+
+
+        // thumbnails
+
+        $thumbnail = false;
+        $thumbnailLarge = false;
+
+        if(is_array($item['pictures']))
+        {
+            foreach($item['pictures'] as $picture)
+            {
+                if($picture['type'] == 'thumbnail')
+                {
+                    // largest thumbnail
+
+                    if(!$thumbnailLarge)
+                    {
+                        $thumbnailLarge = $picture['link'];
+                    }
+
+                    // default thumbnail
+
+                    if(!$thumbnail && $picture['width'] < 640)
+                    {
+                        $thumbnail = $picture['link'];
+                    }
+                }
+            }
+        }
+
+        $video['thumbnail']      = $thumbnail;
+        $video['thumbnailLarge'] = $thumbnailLarge;
+
+        // aliases
+        $video['embedUrl']             = $this->getEmbedUrl($video['id']);
+        $video['embedHtml']            = $this->getEmbedHtml($video['id']);
+        $video['thumbnailSource']      = $video['thumbnail'];
+        $video['thumbnailSourceLarge'] = $video['thumbnailLarge'];
+
+        return $video;
+    }
+
+    private function _queryFromParams($params = array())
+    {
+        $query = array();
+
+        $query['full_response'] = 1;
+
+        if(!empty($params['nextPage']))
+        {
+            $query['page'] = $params['nextPage'];
+            unset($params['nextPage']);
+        }
+        else
+        {
+            $query['page'] = $this->paginationDefaults['page'];
+        }
+
+        $params['nextPage'] = $query['page'] + 1;
+
+        if(!empty($params['q']))
+        {
+            $query['query'] = $params['q'];
+            unset($params['q']);
+        }
+
+        $query['per_page'] = $this->paginationDefaults['perPage'];
+
+        $query = array_merge($query, $params);
+
+        return $query;
+    }
+
+    private function extractCollections($r, $type='album')
+    {
+        $responseCollections = $r;
+
+        $collections = array();
+
+        foreach($responseCollections as $responseCollection)
+        {
+            $collection = $this->{'parseCollection'.ucwords($type)}($responseCollection);
+
+            array_push($collections, $collection);
+        }
+
+        return $collections;
+    }
+}
---
+++ Source/videos/gateways/YouTube.php
@@ -0,0 +1,482 @@
+<?php
+namespace Dukt\Videos\Gateways;
+
+use \Google_Client;
+use \Google_Service_YouTube;
+
+class YouTube extends BaseGateway
+{
+    public $oauthProvider = 'Google';
+    public $oauthScope    = array(
+        'https://www.googleapis.com/auth/userinfo.profile',
+        'https://www.googleapis.com/auth/userinfo.email',
+        'https://www.googleapis.com/auth/youtube',
+        'https://www.googleapis.com/auth/youtube.readonly'
+    );
+
+    // Public Methods
+    // =========================================================================
+
+    public function getName()
+    {
+        return "YouTube";
+    }
+
+    public function getSections()
+    {
+        $sections = array();
+        $sections['Library'] = array(
+            array(
+                'name' => "Uploads",
+                'method' => 'uploads'
+            ),
+            array(
+                'name' => "Favorites",
+                'method' => 'favorites'
+            )
+        );
+
+
+        // playlists
+
+        // try {
+            $playlists = $this->getCollectionsPlaylists();
+        // }
+        // catch(\Exception $e)
+        // {
+        //     // todo: log error
+
+        //     $playlists = false;
+        // }
+
+        if(is_array($playlists))
+        {
+            $items = array();
+
+            foreach($playlists as $playlist)
+            {
+                $item = array(
+                    'name' => $playlist['title'],
+                    'method' => 'playlist',
+                    'options' => array('id' => $playlist['id'])
+                );
+
+                $items[] = $item;
+            }
+
+            if(count($items) > 0)
+            {
+                $sections['Playlists'] = $items;
+            }
+        }
+
+        return $sections;
+    }
+
+    // Protected Methods
+    // =========================================================================
+
+    protected static function getVideoId($url)
+    {
+        // check if url works with this service and extract video_id
+
+        $video_id = false;
+
+        $regexp = array('/^https?:\/\/(www\.youtube\.com|youtube\.com|youtu\.be).*\/(watch\?v=)?(.*)/', 3);
+
+        if(preg_match($regexp[0], $url, $matches, PREG_OFFSET_CAPTURE) > 0)
+        {
+            // regexp match key
+            $match_key = $regexp[1];
+
+            // define video id
+            $video_id = $matches[$match_key][0];
+
+            // Fixes the youtube &feature_gdata bug
+            if(strpos($video_id, "&"))
+            {
+                $video_id = substr($video_id, 0, strpos($video_id, "&"));
+            }
+        }
+
+        // here we should have a valid video_id or false if service not matching
+        return $video_id;
+    }
+
+    protected function getEmbedFormat()
+    {
+        return "https://www.youtube.com/embed/%s?wmode=transparent";
+    }
+
+    protected function getBoolParameters()
+    {
+        return array('autohide', 'cc_load_policy', 'controls', 'disablekb', 'fs', 'modestbranding', 'rel', 'showinfo');
+    }
+
+    protected function getVideo($opts)
+    {
+        if(empty($opts['id']))
+        {
+            throw new \Exception('The video ID is required. (empty found)');
+        }
+
+        $client = $this->getClient();
+        $videos = $client->videos->listVideos('snippet,statistics,contentDetails', array('id' => $opts['id']));
+        $videos = $this->extractVideos($videos);
+
+        if(count($videos) == 1)
+        {
+            return array_pop($videos);
+        }
+        else
+        {
+            throw new \Exception('Video not found');
+        }
+    }
+
+    protected function getVideosUploads($params = array())
+    {
+        return $this->performVideosRequest('uploads', $params);
+    }
+
+    protected function getVideosFavorites($params = array())
+    {
+        // if the account has no playlist or favorites, an exception is thrown.
+        // catch the exception and return response with no videos
+
+        try
+        {
+            return $this->performVideosRequest('favorites', $params);
+        }
+        catch(\Exception $e)
+        {
+           return array(
+                    'videos' => array(),
+                    'more' => false
+                );
+        }
+    }
+
+    protected function getVideosSearch($params = array())
+    {
+        $pagination = $this->_pagination($params);
+
+        $client = $this->getClient();
+
+        $data = array(
+            'q' => $params['q'],
+            'maxResults' => $pagination['perPage']
+        );
+
+        if(!empty($pagination['nextPageToken']))
+        {
+            $data['pageToken'] = $pagination['nextPageToken'];
+        }
+
+        $response = $client->search->listSearch('id', $data);
+
+        foreach($response['items'] as $item)
+        {
+            $videoIds[] = $item->id->videoId;
+        }
+
+        if(!empty($videoIds))
+        {
+            $videoIds = implode(",", $videoIds);
+
+            $videosResponse = $client->videos->listVideos('snippet,statistics,contentDetails', array('id' => $videoIds));
+            $videos = $this->extractVideos($videosResponse);
+
+            $more = false;
+
+            if(!empty($playlistItemsResponse->nextPageToken) && count($videos) > 0)
+            {
+                $more = true;
+            }
+
+            return array(
+                'prevPageToken' => $response->prevPageToken,
+                'nextPageToken' => $response->nextPageToken,
+                'videos' => $videos,
+                'more' => $more
+            );
+        }
+
+        return array();
+    }
+
+    protected function getVideosPlaylist($params = array())
+    {
+        $pagination = $this->_pagination($params);
+
+        $client = $this->getClient();
+
+        $playlistId = $params['id'];
+
+        $data = array(
+            'playlistId' => $playlistId,
+            'maxResults' => $pagination['perPage']
+        );
+
+        if(!empty($pagination['nextPageToken']))
+        {
+            $data['pageToken'] = $pagination['nextPageToken'];
+        }
+
+        $playlistItemsResponse = $client->playlistItems->listPlaylistItems('id,snippet', $data);
+
+        $videoIds = array();
+
+        foreach($playlistItemsResponse['items'] as $item)
+        {
+            $videoId = $item['snippet']['resourceId']['videoId'];
+
+            array_push($videoIds, $videoId);
+        }
+
+        $videoIds = implode(",", $videoIds);
+
+        $videosResponse = $client->videos->listVideos('snippet,statistics,contentDetails', array('id' => $videoIds));
+        $videos = $this->extractVideos($videosResponse);
+
+        $more = false;
+
+        if(!empty($playlistItemsResponse->nextPageToken) && count($videos) > 0)
+        {
+            $more = true;
+        }
+
+        return array(
+                'prevPageToken' => $playlistItemsResponse->prevPageToken,
+                'nextPageToken' => $playlistItemsResponse->nextPageToken,
+                'videos' => $videos,
+                'more' => $more
+            );
+    }
+
+    // Private Methods
+    // =========================================================================
+
+    private function getClient()
+    {
+        // make token compatible with Google library
+        $arrayToken = array();
+        $arrayToken['created'] = 0;
+        $arrayToken['access_token'] = $this->token->accessToken;
+        $arrayToken['expires_in'] = $this->token->endOfLife;
+
+        $arrayToken = json_encode($arrayToken);
+
+
+        // client: client id, secret and redirect uri are fake because we already have a valid token
+        $client = new Google_Client();
+        $client->setApplicationName('Google+ PHP Starter Application');
+        $client->setClientId("clientId");
+        $client->setClientSecret("clientSecret");
+        $client->setRedirectUri("redirectUri");
+
+        $client->setAccessToken($arrayToken);
+
+        $api = new Google_Service_YouTube($client);
+
+        return $api;
+    }
+
+    private function _pagination($params = array())
+    {
+        $pagination = array(
+            'page' => $this->paginationDefaults['page'],
+            'perPage' => $this->paginationDefaults['perPage'],
+            'nextPageToken' => false
+        );
+
+        if(!empty($params['perPage']))
+        {
+            $pagination['perPage'] = $params['perPage'];
+        }
+
+        if(!empty($params['nextPageToken']))
+        {
+            $pagination['nextPageToken'] = $params['nextPageToken'];
+        }
+
+        return $pagination;
+    }
+
+    private function getCollectionsPlaylists($params = array())
+    {
+        $client = $this->getClient();
+
+        $channelsResponse = $client->playlists->listPlaylists('snippet', array(
+          'mine' => 'true',
+        ));
+
+        return $this->extractCollections($channelsResponse['items']);
+    }
+
+    private function parseCollection($item)
+    {
+        $collection = array();
+        $collection['id']          = $item->id;
+        $collection['title']       = $item->snippet->title;
+        $collection['totalVideos'] = 0;
+        $collection['url']         = 'title';
+
+        return $collection;
+    }
+
+    private function parseUser()
+    {
+        $this->id = (string) $response->id;
+        $this->id = substr($this->id, (strpos($this->id, ":user:") + 6));
+        $this->name = (string) $response->author->name;
+    }
+
+    private function parseVideo($item)
+    {
+        $video['raw'] = $item;
+
+        // populate video object
+        $video['gatewayHandle'] = "youtube";
+        $video['gatewayName']   = "YouTube";
+        $video['id']            = $item->id;
+        $video['plays']         = $item->statistics->viewCount;
+        $video['title']         = $item->snippet->title;
+        $video['url']           = 'http://youtu.be/'.$video['id'];
+        $video['authorName']    = $item->snippet->channelTitle;
+        $video['authorUrl']     = "http://youtube.com/channel/".$item->snippet->channelId;
+        $video['date']          = strtotime($item->snippet->publishedAt);
+        $video['description']   = $item->snippet->description;
+
+
+        // thumbnail
+        if(@$item->snippet->thumbnails->medium->url)
+        {
+            $video['thumbnail'] = $item->snippet->thumbnails->medium->url;
+        }
+        elseif(@$item->snippet->thumbnails->default->url)
+        {
+            $video['thumbnail'] = $item->snippet->thumbnails->default->url;
+        }
+
+        // thumbnailLarge
+        if(@$item->snippet->thumbnails->maxres->url)
+        {
+            $video['thumbnailLarge'] = $item->snippet->thumbnails->maxres->url;
+        }
+        elseif(@$item->snippet->thumbnails->high->url)
+        {
+            $video['thumbnailLarge'] = $item->snippet->thumbnails->high->url;
+        }
+        elseif(@$item->snippet->thumbnails->standard->url)
+        {
+            $video['thumbnailLarge'] = $item->snippet->thumbnails->standard->url;
+        }
+        elseif(@$item->snippet->thumbnails->medium->url)
+        {
+            $video['thumbnailLarge'] = $item->snippet->thumbnails->medium->url;
+        }
+        elseif(@$item->snippet->thumbnails->default->url)
+        {
+            $video['thumbnailLarge'] = $item->snippet->thumbnails->default->url;
+        }
+
+        // duration
+        $interval              = new \DateInterval($item->contentDetails->duration);
+        $video['durationSeconds'] = ($interval->d * 86400) + ($interval->h * 3600) + ($interval->i * 60) + $interval->s;
+        $video['duration']        = $this->getDuration($video['durationSeconds']);
+
+        // aliases
+        $video['embedUrl']             = $this->getEmbedUrl($video['id']);
+        $video['embedHtml']            = $this->getEmbedHtml($video['id']);
+        $video['thumbnailSource']      = $video['thumbnail'];
+        $video['thumbnailSourceLarge'] = $video['thumbnailLarge'];
+
+        return $video;
+    }
+
+    private function performVideosRequest($playlist, $params = array())
+    {
+        $pagination = $this->_pagination($params);
+
+        $client = $this->getClient();
+
+        $channelsResponse = $client->channels->listChannels('contentDetails', array(
+          'mine' => 'true',
+        ));
+
+        foreach ($channelsResponse['items'] as $channel)
+        {
+            $uploadsListId = $channel['contentDetails']['relatedPlaylists'][$playlist];
+
+            $data = array(
+                'playlistId' => $uploadsListId,
+                'maxResults' => $pagination['perPage']
+            );
+
+            if(!empty($pagination['nextPageToken']))
+            {
+                $data['pageToken'] = $pagination['nextPageToken'];
+            }
+
+            $playlistItemsResponse = $client->playlistItems->listPlaylistItems('id,snippet', $data);
+
+
+            $videoIds = array();
+
+            foreach($playlistItemsResponse['items'] as $item)
+            {
+                $videoId = $item['snippet']['resourceId']['videoId'];
+
+                array_push($videoIds, $videoId);
+            }
+
+            $videoIds = implode(",", $videoIds);
+
+            $videosResponse = $client->videos->listVideos('snippet,statistics,contentDetails', array('id' => $videoIds));
+            $videos = $this->extractVideos($videosResponse);
+
+            $more = false;
+
+            if(!empty($playlistItemsResponse->nextPageToken) && count($videos) > 0)
+            {
+                $more = true;
+            }
+
+           return array(
+                    'videos' => $videos,
+                    'prevPageToken' => $playlistItemsResponse->prevPageToken,
+                    'nextPageToken' => $playlistItemsResponse->nextPageToken,
+                    'more' => $more
+                );
+        }
+    }
+
+    private function extractVideos($items)
+    {
+        $videos = array();
+
+        foreach($items as $v)
+        {
+            $video = $this->parseVideo($v);
+
+            array_push($videos, $video);
+        }
+
+        return $videos;
+    }
+
+    private function extractCollections($items)
+    {
+        $collections = array();
+
+        foreach($items as $item)
+        {
+            $collection = $this->parseCollection($item);
+
+            array_push($collections, $collection);
+        }
+
+        return $collections;
+    }
+}
