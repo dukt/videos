67476d15d90f89d9aa888b28378d64ddae45334d
Benjamin David <ben@dukt.net>
2015-06-29 09:50:27 +0200

    Simplified BaseGateway::getEmbedUrl() and BaseGateway::getEmbedHtml() methods

--- Source/videos/base/BaseGateway.php
+++ Source/videos/base/BaseGateway.php
@@ -56,74 +56,22 @@ abstract class BaseGateway
             $queryMark = "&";
         }
 
-        $extraParameters = "";
-
-        $disableSize = false;
-
-        if(isset($options['disable_size']))
-        {
-            $disableSize = $options['disable_size'];
-        }
-
-        if(!$disableSize)
-        {
-            if(isset($options['width']))
-            {
-                $width = $options['width'];
-                $extraParameters .= 'width="'.$width.'" ';
-                unset($options['width']);
-            }
-
-            if(isset($options['height']))
-            {
-                $height = $options['height'];
-                $extraParameters .= 'height="'.$height.'" ';
-                unset($options['height']);
-            }
-        }
-
         $options = http_build_query($options);
 
         $format = $this->getEmbedFormat().$queryMark.$options;
 
-        $embed = sprintf($format, $videoId);
+        $embedUrl = sprintf($format, $videoId);
 
-        return $embed;
+        return $embedUrl;
     }
 
     public function getEmbedHtml($videoId, $options = array())
     {
-        $boolParameters = array('disable_size', 'autoplay', 'loop');
-
-        $boolParameters = array_merge($boolParameters, $this->getBoolParameters());
-
-        foreach($options as $k => $o)
-        {
-            foreach($boolParameters as $k2)
-            {
-                if($k == $k2)
-                {
-                    if($o === 1 || $o === "1" || $o === true || $o === "yes")
-                    {
-                        $options[$k] = 1;
-                    }
-
-                    if($o === 0 || $o === "0" || $o === false || $o === "no")
-                    {
-                        $options[$k] = 0;
-                    }
-                }
-            }
-        }
-
-        $queryMark = '?';
-
-        if(strpos($this->getEmbedFormat(), "?") !== false)
-        {
-            $queryMark = "&";
-        }
-
-        $extraParameters = "";
+        $embedAttributes = array(
+            'frameborder' => "0",
+            'allowfullscreen' => "true" ,
+            'allowscriptaccess' => "true"
+        );
 
         $disableSize = false;
 
@@ -136,32 +84,33 @@ abstract class BaseGateway
         {
             if(isset($options['width']))
             {
-                $width = $options['width'];
-                $extraParameters .= 'width="'.$width.'" ';
+                $embedAttributes['width'] = $options['width'];
                 unset($options['width']);
             }
 
             if(isset($options['height']))
             {
-                $height = $options['height'];
-                $extraParameters .= 'height="'.$height.'" ';
+                $embedAttributes['height'] = $options['height'];
                 unset($options['height']);
             }
         }
 
-        $iframeClass = false;
-
         if(!empty($options['iframeClass']))
         {
-            $iframeClass = $options['iframeClass'];
+            $embedAttributes['class'] = $options['iframeClass'];
             unset($options['iframeClass']);
         }
 
-        $options = http_build_query($options);
+        $embedUrl = $this->getEmbedUrl($videoId, $options);
 
-        $embed = '<iframe'.($iframeClass ? ' class="'.$iframeClass.'"' : '').' src="'.sprintf($this->getEmbedFormat().$queryMark.$options, $videoId).'" '.$extraParameters.' frameborder="0" allowfullscreen="true" allowscriptaccess="true"></iframe>';
+        $embedAttributesString = '';
 
-        return $embed;
+        foreach($embedAttributes as $key => $value)
+        {
+            $embedAttributesString .= ' '.$key.'="'.$value.'"';
+        }
+
+        return '<iframe src="'. $embedUrl.'"'.$embedAttributesString.'></iframe>';
     }
 
     public function getVideos($method, $options)
