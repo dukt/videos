f2e472c97fb7f6d1a8c041d07d6120362f5fd864
Benjamin David <ben@dukt.net>
2019-01-30 03:15:03 +0100

    2018 → 2019

--- src/web/twig/variables/VideosVariable.php
+++ src/web/twig/variables/VideosVariable.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @link      https://dukt.net/videos/
- * @copyright Copyright (c) 2018, Dukt
+ * @copyright Copyright (c) 2019, Dukt
  * @license   https://github.com/dukt/videos/blob/v2/LICENSE.md
  */
 
