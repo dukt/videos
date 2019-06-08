c43326d7f6a1c0096e818e85a2b40981f1f435a9
Benjamin David <ben@dukt.net>
2019-06-08 23:53:18 +0200

    Search videos

--- src/web/assets/videosvue/src/js/components/Search.vue
+++ src/web/assets/videosvue/src/js/components/Search.vue
@@ -1,17 +1,52 @@
 <template>
     <div v-if="currentGateway" class="search">
-        <input type="search" class="text fullwidth" :placeholder="'Search '+currentGateway.name+' videos…'"/>
+        <input type="search" class="text fullwidth" v-model="query"
+               :placeholder="'Search '+currentGateway.name+' videos…'"
+               @input="debouncedSearch" @keyup.enter="search"/>
     </div>
 </template>
 
 <script>
+    import debounce from 'lodash.debounce'
+
     import {mapGetters} from 'vuex'
 
     export default {
+        data() {
+            return {
+                query: '',
+            }
+        },
+
         computed: {
             ...mapGetters([
                 'currentGateway',
             ]),
+            
+            debouncedSearch() {
+                return debounce(() => {
+                    this.search()
+                }, 1000)
+            }
+        },
+
+        methods: {
+            search() {
+                this.debouncedSearch.cancel()
+
+                this.$store.commit('updateSelectedCollection', null)
+
+                this.$store.dispatch('getVideos', {
+                        gateway: this.currentGateway.handle,
+                        method: 'search',
+                        options: {
+                            q: this.query
+                        },
+                    })
+                    .catch((error) => {
+                        this.$store.dispatch('displayError', 'Couldn’t get videos.')
+                    })
+            }
         }
     }
 </script>
