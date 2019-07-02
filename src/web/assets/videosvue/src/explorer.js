accb6b41aba0942378b07583cd23f70168a055cd
Benjamin David <ben@dukt.net>
2019-07-02 21:20:45 +0200

    Renamed `App` component to `Explorer`

--- src/web/assets/videosvue/src/explorer.js
+++ src/web/assets/videosvue/src/explorer.js
@@ -1,16 +1,16 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Explorer from './Explorer.vue'
 import store from './js/store'
 import videosApi from './js/api/videos'
 
 Vue.config.productionTip = false
 
 // new Vue({
-//     render: h => h(App),
+//     render: h => h(Explorer),
 //     store,
 // }).$mount('.videos-vue-app')
 
 window.VideoExplorerConstructor = Vue.extend({
-    render: h => h(App),
+    render: h => h(Explorer),
     store,
 })
