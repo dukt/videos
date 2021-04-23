e76caf1df6ca731c7bffc610b3d1f07e4d87b185
Benjamin David <ben@dukt.net>
2021-04-23 19:03:32 +0200

    Added missing video duration

--- src/web/assets/videos/src/js/components/VideoCard.vue
+++ src/web/assets/videos/src/js/components/VideoCard.vue
@@ -5,6 +5,7 @@
         }]">
             <img :src="video.thumbnail" :alt="video.title">
             <div class="play" @click="play(video)"></div>
+            <div class="duration">{{video.duration}}</div>
         </div>
         <div class="mt-2 line-clamp-2">{{video.title}}</div>
     </div>
