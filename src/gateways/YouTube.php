64eb252171abc4e7dfb090d43d17b37884c60fa8
josdigital <128366984+josdigital@users.noreply.github.com>
2023-12-09 11:37:43 +0100

    Remove Youtube broad scope
    The scope removed was asking the user permissions to manage the FULL Youtube account and that was not needed. In fact the read.only scope is more than enough to get the permission token to view the user plugin. The extra scope has been removed because when submitting a custom google app for verification, Google will complain of this large scope (and this also gives too much power to this plugin, with the risk of compromising the youtube account of the user)

--- src/gateways/YouTube.php
+++ src/gateways/YouTube.php
@@ -75,7 +75,6 @@ class YouTube extends Gateway
         return [
             'https://www.googleapis.com/auth/userinfo.profile',
             'https://www.googleapis.com/auth/userinfo.email',
-            'https://www.googleapis.com/auth/youtube',
             'https://www.googleapis.com/auth/youtube.readonly'
         ];
     }
